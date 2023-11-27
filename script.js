382388f209c23cda844f9f52b61188adce6eac52
Tyler-Cav <Tyler.Cavanaugh24@gmail.com>
2023-11-27 18:03:16 -0500

    on click function added as well as additional empty arrays to store user selection and user password that's generated

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ let upperCaseAlphabet = Array.from('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
 let lowerCaseAlphabet = Array.from("abcdefghijklmnopqrstuvwxyz");
 let numeric = Array.from('1234567890')
 let special_characters = Array.from("!@#$%^&*?<>/");
+
+generateBtn.onclick = () => {
+
+    let UsersPasswordArray = [];
+    let FinalUserPassword = [];
