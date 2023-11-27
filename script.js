4fd85293f5699d23d3be46404d693afc6c640d1e
Tyler-Cav <Tyler.Cavanaugh24@gmail.com>
2023-11-27 18:04:58 -0500

    Created comparison statements for user prompts

--- script.js
+++ script.js
@@ -9,3 +9,26 @@ generateBtn.onclick = () => {
 
     let UsersPasswordArray = [];
     let FinalUserPassword = [];
+
+    let numberOfCharactersPrompt = window.prompt("Welcome to Password Generator. In the prompt below, please type the number of characters you would like within your password. Please note it must be a minimum of 8 and a maxmium of 128.");
+        if (numberOfCharactersPrompt >= 8 && numberOfCharactersPrompt <= 128) {
+                console.log(numberOfCharactersPrompt);
+                window.alert("Great! Next, let's select the types of characters you want. You MUST select at least one type from the four options. Please select OK for yes or Cancel for no with what characters you'd potentially like in your password!"); 
+                let promptUppercase = window.confirm("Uppercase Letters?");
+                if (promptUppercase === true) 
+                        UsersPasswordArray = UsersPasswordArray.concat(upperCaseAlphabet);
+                let promptLowerCase = window.confirm("Lowercase Letters?");
+                if (promptLowerCase === true) 
+                        UsersPasswordArray = UsersPasswordArray.concat(lowerCaseAlphabet);
+                let numericPrompt = window.confirm("Numerics?");
+                if (numericPrompt === true) {
+                        UsersPasswordArray = UsersPasswordArray.concat(numeric);
+                        console.log(UsersPasswordArray)
+                } else {
+                        console.log(UsersPasswordArray)        
+                }
+                let specialPrompt = window.confirm("Special? (Ex: !@#($)?");
+                if (specialPrompt === true) {
+                        UsersPasswordArray = UsersPasswordArray.concat(special_characters);
+                        console.log(UsersPasswordArray)
+                } 
