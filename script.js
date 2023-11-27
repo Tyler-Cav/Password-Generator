55954db06fbcb46b833484075315a0884fb2846f
Tyler-Cav <Tyler.Cavanaugh24@gmail.com>
2023-11-27 18:08:55 -0500

    removed console logs and else statements

--- script.js
+++ script.js
@@ -15,20 +15,18 @@ generateBtn.onclick = () => {
                 console.log(numberOfCharactersPrompt);
                 window.alert("Great! Next, let's select the types of characters you want. You MUST select at least one type from the four options. Please select OK for yes or Cancel for no with what characters you'd potentially like in your password!"); 
                 let promptUppercase = window.confirm("Uppercase Letters?");
-                if (promptUppercase === true) 
-                        UsersPasswordArray = UsersPasswordArray.concat(upperCaseAlphabet);
+                if (promptUppercase === true) {
+                        UsersPasswordArray = UsersPasswordArray.concat(upperCaseAlphabet)
+                    }
                 let promptLowerCase = window.confirm("Lowercase Letters?");
-                if (promptLowerCase === true) 
+                if (promptLowerCase === true) {
                         UsersPasswordArray = UsersPasswordArray.concat(lowerCaseAlphabet);
+                    }
                 let numericPrompt = window.confirm("Numerics?");
                 if (numericPrompt === true) {
                         UsersPasswordArray = UsersPasswordArray.concat(numeric);
-                        console.log(UsersPasswordArray)
-                } else {
-                        console.log(UsersPasswordArray)        
                 }
                 let specialPrompt = window.confirm("Special? (Ex: !@#($)?");
                 if (specialPrompt === true) {
                         UsersPasswordArray = UsersPasswordArray.concat(special_characters);
-                        console.log(UsersPasswordArray)
-                } 
+                }
