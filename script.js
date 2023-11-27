377300320a5395f061ca781ca753fc3e05872307
Tyler-Cav <Tyler.Cavanaugh24@gmail.com>
2023-11-27 18:01:45 -0500

    added arrays for user prompts

---
+++ script.js
@@ -0,0 +1,6 @@
+var generateBtn = document.querySelector("#generate");
+
+let upperCaseAlphabet = Array.from('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+let lowerCaseAlphabet = Array.from("abcdefghijklmnopqrstuvwxyz");
+let numeric = Array.from('1234567890')
+let special_characters = Array.from("!@#$%^&*?<>/");
